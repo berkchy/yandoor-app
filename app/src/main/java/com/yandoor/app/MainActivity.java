3b293c8071bd2e1213bb22a78004eae8df362a61
Berk <berkchcode@gmail.com>
2026-03-14 21:14:03 +0300

    Add files via upload

---
+++ app/src/main/java/com/yandoor/app/MainActivity.java
@@ -0,0 +1,22 @@
+package com.yandoor.app;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://yandoor.vercel.app/");
+    }
+}
