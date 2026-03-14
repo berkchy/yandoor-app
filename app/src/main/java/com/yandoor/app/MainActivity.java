71150be9c91db8329c499639eb8c2b6e413dbdb6
Berk <berkchcode@gmail.com>
2026-03-15 02:15:05 +0300

    Update MainActivity.java

--- app/src/main/java/com/yandoor/app/MainActivity.java
+++ app/src/main/java/com/yandoor/app/MainActivity.java
@@ -1,7 +1,11 @@
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,26 +16,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        webView = new WebView(this);
-        setContentView(webView);
+        try {
+            webView = new WebView(this);
+            setContentView(webView);
 
-        WebSettings settings = webView.getSettings();
+            WebSettings settings = webView.getSettings();
+            settings.setJavaScriptEnabled(true);
+            settings.setDomStorageEnabled(true);
+            settings.setLoadsImagesAutomatically(true);
+            settings.setAllowFileAccess(true);
+            settings.setAllowContentAccess(true);
+            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
 
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setLoadsImagesAutomatically(true);
-        settings.setAllowFileAccess(true);
-        settings.setAllowContentAccess(true);
-        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+            webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                    super.onReceivedError(view, request, error);
+                    Toast.makeText(MainActivity.this, "WebView Error: " + error.getDescription(), Toast.LENGTH_LONG).show();
+                    Log.e("CRASH_DEBUG", "WebView Error: ", new Exception(error.getDescription().toString()));
+                }
+            });
 
-        webView.setWebViewClient(new WebViewClient());
+            webView.loadUrl("https://yandoor.vercel.app/");
 
-        webView.loadUrl("https://yandoor.vercel.app/");
+        } catch (Exception e) {
+            Toast.makeText(this, "Crash: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            Log.e("CRASH_DEBUG", "Crash detay: ", e);
+        }
     }
 
     @Override
     public void onBackPressed() {
-        if (webView.canGoBack()) {
+        if (webView != null && webView.canGoBack()) {
             webView.goBack();
         } else {
             super.onBackPressed();
